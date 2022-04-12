f23062a4adb8611d8cc337007f5f599467b27f11
David Abol Ger <draker74@gmail.com>
2022-04-13 01:29:36 +0300

    all data

--- style.js
+++ style.js
@@ -3,13 +3,13 @@ const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 
 function show() {
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
 }
 
 function close() {
-    mainMenu.style.top = '-100vh';
-    mainMenu.style.display = 'none';
+  mainMenu.style.top = '-100vh';
+  mainMenu.style.display = 'none';
 }
 
 openMenu.addEventListener('click', show);
@@ -21,13 +21,13 @@ const dispNone = document.querySelector('.dis-none1');
 const lessButton = document.querySelector('.show-less');
 
 function showB() {
-    dispNone.style.display = 'block';
-    moreButton.style.display = 'none';
+  dispNone.style.display = 'block';
+  moreButton.style.display = 'none';
 }
 
 function hideB() {
-    dispNone.style.display = 'none';
-    moreButton.style.display = 'block';
+  dispNone.style.display = 'none';
+  moreButton.style.display = 'block';
 }
 
 moreButton.addEventListener('click', showB);
